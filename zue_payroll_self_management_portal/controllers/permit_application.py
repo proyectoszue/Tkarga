604f0d57ea1663a386de463843c983450340be80
Buitron98 <luis.buitron.zue@outlook.com>
2022-07-08 10:57:07 -0500

    Portal de nómina - Solicitud de permisos.

--- zue_payroll_self_management_portal/controllers/permit_application.py
+++ zue_payroll_self_management_portal/controllers/permit_application.py
@@ -9,6 +9,7 @@ from PyPDF2 import  PdfFileReader, PdfFileWriter
 
 from odoo.http import request, route, Controller
 from odoo.tools.safe_eval import safe_eval
+from odoo.tools import float_round
 
 
 class ZuePermitApplicationPortal(Controller):
@@ -27,18 +28,42 @@ class ZuePermitApplicationPortal(Controller):
             return request.not_found()
 
         obj_employee = request.env['hr.employee.public'].search([('user_id', '=', request.env.user.id)], limit=1)
-        
+
+        if post.get('initial_hour',False):
+            initial_hour_lst = post['initial_hour'].split(':')
+            initial_hour = float_round(int(initial_hour_lst[0]) + int(initial_hour_lst[1]) / 60 + 0 / 3600, precision_digits=2)
+        else:
+            initial_hour = 0
+
+        if post.get('final_hour',False):
+            final_hour_lst = post['final_hour'].split(':')
+            final_hour = float_round(int(final_hour_lst[0]) + int(final_hour_lst[1]) / 60 + 0 / 3600, precision_digits=2)
+        else:
+            final_hour = 0
+
         permit = {
             'employee_id' : obj_employee.id,
             'permit_date' : post['permit_date'],
-            'permit_days' : post['permit_days'],
-            'reason' : post['reason'],
-            'observation' : post['observation'],
-            'initial_hour' : post['initial_date'],
-            'final_hour' : post['final_date'],
-            'compensated' : 0
+            'reason': post['reason'],
+            'text_other': post.get('text_other',''),
+            'leave_requested_more_day': True if post.get('leave_requested_more_day',False) == '' else False,
+            'permit_days' : int(post.get('permit_days',0)),
+            'initial_hour' : initial_hour,
+            'final_hour' : final_hour,
+            'observation': post['observation'],
+            'state': 'confirm'
         }
 
-        request.env['hr.permit.application'].create(permit)
+        request.env['hr.permit.application'].sudo().create(permit)
 
         return request.redirect('/zue_payroll_self_management_portal')
+
+    @route(["/zue_payroll_self_management_portal/my_application_permits"], type='http', auth='user', website=True)
+    def my_application_permits(self, **kw):
+        if not request.env.user:
+            return request.not_found()
+
+        obj_employee = request.env['hr.employee.public'].search([('user_id', '=', request.env.user.id)], limit=1)
+        obj_permit_application = request.env['hr.permit.application'].search([('employee_id', '=', obj_employee.id)])
+
+        return request.render('zue_payroll_self_management_portal.my_application_permits', {'obj_employee': obj_employee,'obj_permit_application':obj_permit_application})
