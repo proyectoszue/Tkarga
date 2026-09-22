b8109ba8d69ca0a79796fd642544d681873551ba
BarretoAngel <121039563+BarretoAngel@users.noreply.github.com>
2026-09-22 10:44:42 -0500

    [FIX] Validación de warnings en lotes de nómina

--- zue_hr_payroll/__manifest__.py
+++ zue_hr_payroll/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Human Resources/Payroll',
-    'version': '19.0.1.4.0',
+    'version': '19.0.1.4.3',
     'icon': '/zue_hr_payroll/static/description/icon.png',
 
     # any module necessary for this one to work correctly
