6d2f754a6f05559e6c10236284563461085cfca1
BarretoAngel <121039563+BarretoAngel@users.noreply.github.com>
2026-09-23 11:34:39 -0500

    [FIX] #15632 Filtrar documentos por empleado seleccionado

--- zue_documents/models/documents_document.py
+++ zue_documents/models/documents_document.py
@@ -9,3 +9,18 @@ class documents_document(models.Model):
     activity_note = fields.Html(string="Message")
     full_url = fields.Char(string="URL")
     date_deadline = fields.Date('Expected Closing')
+
+    def get_formview_action(self, access_uid=None):
+        # Enlace Many2one: misma carpeta del archivo + solo documentos del partner (empleado)
+        self.ensure_one()
+        if not self.partner_id:
+            return super().get_formview_action(access_uid=access_uid)
+        action = self.env['ir.actions.actions']._for_xml_id('documents.document_action_preference')
+        context = {
+            'default_partner_id': self.partner_id.id,
+            'searchpanel_default_user_folder_id': str(self.folder_id.id) if self.folder_id else False,
+        }
+        return action | {
+            'domain': [('partner_id', '=', self.partner_id.id)],
+            'context': context,
+        }
